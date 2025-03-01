78a4bdb5b4cd6776f29f92e1ad90049259a8c962
OM3X4 <om3x4e@gmail.com>
2025-03-01 23:22:27 +0200

    omar

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({  plugins: [    tailwindcss(),  ], base:'/'})
+export default defineConfig({  plugins: [    tailwindcss(),  ], base:'/EgyptProjectsHost/'})
